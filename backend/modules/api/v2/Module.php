d3ecfe446cfa47a3ee2ed590b462d15c6831245e
NoicFank <1015542478@qq.com>
2017-11-14 15:23:51 +0800

    初始文件

---
+++ backend/modules/api/v2/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\api\v2;
+
+/**
+ * v2 module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'backend\modules\api\v2\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
